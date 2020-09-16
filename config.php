3ed2f18ab5fe39b22abda1bd8463412321c84457
kudutenang <66642574+kudutenang@users.noreply.github.com>
2020-09-17 05:55:25 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?
+
+$token = "6fd48de6f3ffae1e87cda32df29c5ffbfe17e565";
+$PHPSESSID = "m15uffk6s1g9n3c0dgbuf6gqg3";
+$cfduid = "dec56f61c12295c48391f7f2e13b808221600240997";
+$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36";
